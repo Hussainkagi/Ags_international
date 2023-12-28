b79c24abaf505f30f1b6c0ea075fae972a8ef7e6
HussainKagiwala <hussain.k@techovarya.com>
2023-12-28 21:04:40 +0400

    Home

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
-import Home from './components/Home/Home'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
